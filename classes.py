fbba09b3076a66a543e5ba8a3c248729e5f818a0
AshfordG <ashford.gavin@menloschool.org>
2023-12-12 12:50:12 -0800

    Final push to github before taking late

--- classes.py
+++ classes.py
@@ -21,7 +21,7 @@ class Player:
     def __init__(self):
         self.pos = [300, 300]
         self.radius = 15
-        self.speed = 5
+        self.speed = 2
 
     def move(self, keys):
         if keys[pygame.K_LEFT] and self.pos[0] > self.radius:
@@ -85,22 +85,44 @@ class Game:
         self.clock = pygame.time.Clock()
         self.player = Player()
         self.ai = AI()
+        # self.fsm = FSM(self.medium)
 
         self.player_image = pygame.image.load('pixelMan.png')
         self.player_image = pygame.transform.scale(self.player_image, (self.player.radius * 2, self.player.radius * 2))
+        # self.scale_factor = 1.25
+        # self.player_image = pygame.transform.scale(self.player_image, (self.player.radius * 2 * self.scale_factor, self.player.radius * 2 * self.scale_factor))
         
         self.ai_image = pygame.image.load('dogName.png')
         self.ai_image = pygame.transform.scale(self.ai_image, (self.ai.radius * 2, self.ai.radius * 2))
 
+        self.car_speed = 3  # Adjust the speed of the cars as needed
+        self.cars = []  # List to store car positions
+
+        # Add initial positions of cars
+        for i in range(100, 600, 150,):
+            self.cars.append(pygame.Rect(i, i - 20, 30, 30))
+
         self.walls = [
-            pygame.Rect(100, 50, 20, 200),
-            pygame.Rect(250, 150, 300, 20),
-            pygame.Rect(70, 400, 20, 150),
-            pygame.Rect(400, 350, 20, 150),
             pygame.Rect(0, 0, 600, 15),
             pygame.Rect(0, 0, 15, 600),
             pygame.Rect(0, 585, 600, 14),
-            pygame.Rect(585, 0, 15, 600)
+            pygame.Rect(585, 0, 15, 600),
+            pygame.Rect(325, 270, 20, 70),
+            pygame.Rect(260, 270, 20, 70),
+            pygame.Rect(0, 270, 100, 20),
+            pygame.Rect(160, 270, 120, 20),
+            pygame.Rect(325, 270, 110, 20),
+            pygame.Rect(490, 270, 120, 20),
+            pygame.Rect(0, 270, 120, 20), 
+            pygame.Rect(65, 65,200, 20),
+            pygame.Rect(330, 65, 200, 20),
+            pygame.Rect(330, 65, 20, 50),
+            pygame.Rect(265, 65, 20, 50),
+            pygame.Rect(150, 155, 300, 20),
+            pygame.Rect(330, 65, 20, 50),
+
+
+            
         ]
         self.grass_color = (124, 252, 0)
         self.wall_color = (169, 169, 169)
@@ -135,6 +157,35 @@ class Game:
                     self.ai.pos[1] +=  2 * self.ai.speed
                 if self.ai.current_direction == 'DOWN':
                     self.ai.pos[1] -= 2 *  self.ai.speed
+        
+        if player_rect.colliderect(ai_rect):
+            return True
+
+    def move_cars(self):
+        for car in self.cars:
+            car.x += self.car_speed
+            if car.right > WIDTH: 
+                car.left = 0
+
+    def draw_cars(self):
+        for car in self.cars:
+            pygame.draw.rect(self.win, (255, 0, 0), car)
+
+    def check_car_collision(self):
+        player_rect = pygame.Rect(self.player.pos[0] - self.player.radius, self.player.pos[1] - self.player.radius, self.player.radius * 2, self.player.radius * 2)
+        
+        for car in self.cars:
+            if player_rect.colliderect(car):
+                return True
+        return False
+
+    def end_game(self):
+        font = pygame.font.Font(None, 36)
+        text = font.render('Game Over', True, (255, 0, 0))
+        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        self.win.blit(text, text_rect)
+        pygame.display.flip()
+        pygame.time.wait(2000)
 
     def run(self):
         running = True
@@ -146,6 +197,16 @@ class Game:
             self.player.move(keys)
             self.ai.move()
             self.check_collisions(keys)
+            self.move_cars()
+            self.draw_cars()
+
+            car_collision = self.check_car_collision()  # Check collision with cars
+
+            if car_collision:
+                self.player.pos[0] = 300
+                self.player.pos[1] = 300
+                # running = False
+                # self.end_game()
 
             for wall in self.walls:
                 pygame.draw.rect(self.win, self.wall_color, wall)
@@ -153,8 +214,10 @@ class Game:
             self.win.blit(self.player_image, (self.player.pos[0] - self.player.radius, self.player.pos[1] - self.player.radius))
             self.win.blit(self.ai_image, (self.ai.pos[0] - self.ai.radius, self.ai.pos[1] - self.ai.radius))
 
-            #pygame.draw.circle(self.win, YELLOW, self.player.pos, self.player.radius)
-            #pygame.draw.circle(self.win, DOG, self.ai.pos, self.ai.radius)
+            collision = self.check_collisions(keys)
+            if collision:
+                running = False
+                self.end_game()
             
             pygame.display.flip()
             self.clock.tick(FPS)
